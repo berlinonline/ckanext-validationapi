0ac39afdb8144b09d5f34026becd24f2f31fb95e
Knud Möller <knud@datalysator.com>
2018-11-07 13:25:55 +0100

    add more tests, streamline test structure

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-nosetests -s --ckan --with-pylons=test.ini --with-coverage --cover-package=ckanext.validationapi --cover-inclusive --cover-erase --cover-html ckanext/validationapi/tests/
+nosetests -s --ckan --with-pylons=test.ini --with-coverage --cover-package=ckanext.validationapi --cover-erase --cover-html ckanext/validationapi/tests/
